baa58843ff946debda3933e092f457bfeab5c5f5
David <David@rootsecurity.nl>
2023-08-20 20:36:20 +0200

    Adding little weather lookup phy script

---
+++ miniproject/app.py
@@ -0,0 +1,5 @@
+# app.py 
+api_key = "12abc3d45ef6789012345g6789h0ij12" 
+city_id = "5128581" 
+base_url = "https://api.openweathermap.org/data/2.5/weather?" 
+final_url = base_url + "appid=" + api_key + "&id=" + city_id
