2642fcc78561e5d53409f33b647d863319480357
David <David@rootsecurity.nl>
2024-01-05 19:05:46 +0100

    SAS

--- Blobconnect.py
+++ Blobconnect.py
@@ -1,7 +1,7 @@
 def get_blob_service_client_account_key(self):
     # TODO: Replace <storage-account-name> with your actual storage account name
     account_url = "https://aiaprojectxstorage.blob.core.windows.net"
-    shared_access_key = "gtrbirx4XyTqmAU0Gu8YoFDJay8NHqWQqTLi+4LBnBWuqHY5mR1smotzRBx7ztjtj/xTB57N+lxm+AStBdydDg=="
+    shared_access_key = "XuXouuXxRE/LBBpDvc3cp8jS0ufBdKfrjwdrHv1/sb7EiDquK5x002CkMQYBdy33NR+Pvg3oW/i5+AStJSq/vA=="
     credential = shared_access_key
 
     # Create the BlobServiceClient object
